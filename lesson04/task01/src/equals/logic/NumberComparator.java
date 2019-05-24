5152c4965089b0f3a4faa950ebb4e2790491bb42
NikolayTsikunov <50696531+NikolayTsikunov@users.noreply.github.com>
2019-05-24 22:00:57 +0300

    Update NumberComparator.java

--- lesson04/task01/src/equals/logic/NumberComparator.java
+++ lesson04/task01/src/equals/logic/NumberComparator.java
@@ -2,6 +2,6 @@ package equals.logic;
 
 public class NumberComparator {
     public static boolean isNumbersEqual (double a, double b, double c) {
-        return a == b && b == c;
+        return (a == b && b == c);
     }
 }
