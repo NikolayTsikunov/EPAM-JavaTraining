5a5a7ca74f96d496cdbf8635a9872ccd424ead40
NikolayTsikunov <50696531+NikolayTsikunov@users.noreply.github.com>
2019-05-24 22:22:24 +0300

    Update Digits.java
    Removed if-else constructions

--- lesson04/task03/src/check/logic/Digits.java
+++ lesson04/task03/src/check/logic/Digits.java
@@ -2,24 +2,10 @@ package check.logic;
 
 public class Digits {
     public static boolean isDigitsRise(int one, int two, int three, int four) {
-
-        boolean equals = false;
-
-        if(one < two && two < three && three < four) {
-            equals = true;
-        }
-
-        return equals;
+        return one < two && two < three && three < four;
     }
 
     public static boolean isDigitsDecrease(int one, int two, int three, int four) {
-
-        boolean equals = false;
-
-        if(one > two && two > three && three > four) {
-            equals = true;
-        }
-
-        return equals;
+        return one > two && two > three && three > four;
     }
 }
