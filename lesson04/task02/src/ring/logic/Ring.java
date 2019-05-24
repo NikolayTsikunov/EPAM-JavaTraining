cc7f856ce7ca4e8d8f1d6fc148e5193f020506d4
NikolayTsikunov <50696531+NikolayTsikunov@users.noreply.github.com>
2019-05-24 22:29:22 +0300

    Update Ring.java
    Remove if-else constructions

--- lesson04/task02/src/ring/logic/Ring.java
+++ lesson04/task02/src/ring/logic/Ring.java
@@ -2,10 +2,6 @@ package ring.logic;
 
 public class Ring {
     public static double calculateSquare(double squareOne, double squareTwo) {
-        if(squareOne == squareTwo || squareOne == 0 || squareTwo == 0) {
-            return 0;
-        } else {
-            return Math.abs(squareOne - squareTwo);
-        }
+        return (squareOne == 0 || squareTwo == 0) ? 0 : Math.abs(squareOne - squareTwo);
     }
 }
