dd4178b3ef01ff780d5a582ebf712a4d1547fd09
NikolayTsikunov <50696531+NikolayTsikunov@users.noreply.github.com>
2019-05-24 22:24:09 +0300

    Update Circle.java
    Removed if-else constructions

--- lesson04/task02/src/ring/logic/Circle.java
+++ lesson04/task02/src/ring/logic/Circle.java
@@ -2,10 +2,6 @@ package ring.logic;
 
 public class Circle {
     public static double calculateSquare(int radius) {
-        if(radius == 0) {
-            return 0;
-        } else {
-            return Math.PI * Math.pow(radius, 2);
-        }
+        return Math.PI * Math.pow(radius, 2);
     }
 }
