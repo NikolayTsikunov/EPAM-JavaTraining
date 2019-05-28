fa1cb2291c16c7a6c68d46ae20c1cbf648d900d7
Nikolay Tsikunov <nikolay.tsikunov@mail.ru>
2019-05-28 17:39:10 +0300

    Remade task02

--- lesson04/task01/src/equals/logic/NumberComparator.java
+++ lesson04/task01/src/equals/logic/NumberComparator.java
@@ -2,11 +2,6 @@ package equals.logic;
 
 public class NumberComparator {
     public static boolean isNumbersEqual (double a, double b, double c) {
-        if(a == b && b == c) {
-            return true;
-        } else {
-            return false;
-        }
+        return (a == b && b == c);
     }
-
 }
