060df6eb2614db86d34c5db61578ea1d9f16a6da
Nikolay Tsikunov <nikolay.tsikunov@mail.ru>
2019-05-28 18:18:08 +0300

    Update task03

--- lesson04/task03/src/check/logic/Digits.java
+++ lesson04/task03/src/check/logic/Digits.java
@@ -1,27 +1,35 @@
 package check.logic;
 
 public class Digits {
-    final private static int ONE = 1;
-    final private static int TWO = 2;
-    final private static int TREE = 3;
-    final private static int FOUR = 4;
 
     public static boolean isDigitsRise(int number) {
 
-        int first = ParsingNumbers.takeDigit(number, FOUR);
-        int second = ParsingNumbers.takeDigit(number, TREE);
-        int third = ParsingNumbers.takeDigit(number, TWO);
-        int fourth = ParsingNumbers.takeDigit(number, ONE);
+        int fourth = number % 10;
+        number /= 10;
+
+        int third = number % 10;
+        number /= 10;
+
+        int second = number % 10;
+        number /= 10;
+
+        int first = number;
 
         return first < second && second < third && third < fourth;
     }
 
     public static boolean isDigitsDecrease(int number) {
 
-        int first = ParsingNumbers.takeDigit(number, FOUR);
-        int second = ParsingNumbers.takeDigit(number, TREE);
-        int third = ParsingNumbers.takeDigit(number, TWO);
-        int fourth = ParsingNumbers.takeDigit(number, ONE);
+        int fourth = number % 10;
+        number /= 10;
+
+        int third = number % 10;
+        number /= 10;
+
+        int second = number % 10;
+        number /= 10;
+
+        int first = number;
 
         return first > second && second > third && third > fourth;
     }
